3e213f1b3f744f7ff1fed457db8ad6d826e34c3d
zac <ZACL0001@e.ntu.edu.sg>
2026-04-10 18:50:58 +0800

    update

---
+++ heartbleed-demo/client.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 9090
+
+int main() {
+    WSADATA wsa;
+    SOCKET sock;
+    struct sockaddr_in server;
+
+    unsigned short payload;
+    char data[1024];
+    char buffer[2048];
+
+    // Initialize Winsock
+    WSAStartup(MAKEWORD(2,2), &wsa);
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    connect(sock, (struct sockaddr*)&server, sizeof(server));
+
+    printf("Enter payload length: ");
+    scanf("%hu", &payload);
+
+    printf("Enter data: ");
+    scanf("%s", data);
+
+    // Build packet: [length][data]
+    memcpy(buffer, &payload, sizeof(unsigned short));
+    memcpy(buffer + sizeof(unsigned short), data, strlen(data));
+
+    send(sock, buffer, sizeof(unsigned short) + strlen(data), 0);
+
+    // Receive response
+    int len = recv(sock, buffer, sizeof(buffer), 0);
+
+    printf("\nReceived (%d bytes):\n", len);
+
+    // Print safely (including non-printable chars)
+    for (int i = 0; i < len; i++) {
+        printf("%c", buffer[i]);
+    }
+    printf("\n");
+
+    closesocket(sock);
+    WSACleanup();
+
+    return 0;
+}
---
+++ heartbleed-demo/server_fixed.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 9090
+
+int main() {
+    WSADATA wsa;
+    SOCKET server_fd, client_sock;
+    struct sockaddr_in server, client;
+    int c = sizeof(client);
+
+    struct {
+        char buffer[1024];
+        char secret[32];
+    } memory;
+
+    strcpy(memory.secret, "SUPER_SECRET_KEY_12345");
+
+    char response[2048];
+
+    WSAStartup(MAKEWORD(2,2), &wsa);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
+
+    bind(server_fd, (struct sockaddr*)&server, sizeof(server));
+    listen(server_fd, 1);
+
+    printf("Fixed server listening on port %d...\n", PORT);
+    printf("Secret in memory: %s\n", memory.secret); // debug
+
+    while (1) {
+
+        client_sock = accept(server_fd, (struct sockaddr*)&client, &c);
+        if (client_sock == INVALID_SOCKET) continue;
+
+        printf("Client connected\n");
+
+        while (1) {
+            memset(memory.buffer, 0, sizeof(memory.buffer));
+
+            int received = recv(client_sock, memory.buffer, sizeof(memory.buffer), 0);
+
+            if (received <= 0) {
+                printf("Client disconnected\n");
+                break;
+            }
+
+            unsigned short payload;
+            memcpy(&payload, memory.buffer, sizeof(unsigned short));
+
+            char *data = memory.buffer;
+
+            printf("Received payload length: %hu\n", payload);
+
+            // PATCH: prevent over-read
+            int data_len = received - sizeof(unsigned short);
+            if (payload > data_len || payload > (int)sizeof(response)) {
+                printf("Blocked: invalid payload (possible attack)\n");
+                continue;
+            }
+
+            memcpy(response, data, payload);
+            send(client_sock, response, payload, 0);
+        }
+
+        closesocket(client_sock);
+    }
+
+    closesocket(server_fd);
+    WSACleanup();
+
+    return 0;
+}
